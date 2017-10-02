c9d5862da63fa7d8f3e76b60d58337fc8cdd7ace
julian1704 <rjacosta@uninorte.edu.co>
2017-10-02 17:23:47 -0500

    primera app xamarin

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+	public partial class MainPage : ContentPage
+	{
+        public MainPage()
+        {
+            var layout = new StackLayout();
+            var label = new Label { Text = "Ingresar", };
+            var entry = new Entry { Placeholder = "Nombre", };
+            var button = new Button
+            { Text = "Presionar", };
+            button.Clicked += Button_Clicked;
+
+            Content = layout;
+            layout.Children.Add(label);
+            layout.Children.Add(entry);
+            layout.Children.Add(button);
+
+            
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Atención","El botón ha sido presionado","Ok");
+        }
+
+    }
+ }
+
